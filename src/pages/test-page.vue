d92a03b879eb69099e11e4cf08a411cddc5c09a7
mutekki <dk@tfl.io>
2022-09-28 12:57:52 +0300

    test

--- src/pages/test-page.vue
+++ src/pages/test-page.vue
@@ -3,3 +3,12 @@
         <h1>это тестовая страница</h1>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'test-page',
+    created() {
+        this.$store.dispatch('test')
+    }
+}
+</script>
